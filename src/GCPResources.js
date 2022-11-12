b7aea0a2e2f03e49679497640dfacbfdfe679cd3
Malo10LeGoff <malo.legoff@datadoghq.com>
2022-11-12 20:24:35 +0100

    first link backend/frontend

--- src/GCPResources.js
+++ src/GCPResources.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./GCPResources.css";
 import SideBar from "./SideBar";
 
 function GCPResources() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:8080")
+      .then((data) => {
+        console.log(data.json());
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <div className="gcp_resources">
       <SideBar></SideBar>
--- src/Homepage.js
+++ src/Homepage.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./Homepage.css";
 import Header from "./Header";
 
-function sayHello() {
-  alert("You clicked me!");
-}
+function sayHello() {}
 
 function Homepage() {
   return (
     <div className="homepage">
       <Header></Header>
       <div>
-        <a className="gcp" href="https://react.school" target="_blank">
+        <a className="gcp" href="./gcp_resources">
           <button className="gcp_button" onClick={sayHello}>
             Start monitoring your GCP energy consumption
           </button>
